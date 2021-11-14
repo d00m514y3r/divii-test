4dc5ccbfc7ef06adf0622c49d56f87fa4f8bd5a9
r3tr0 <Parsashowkati@gmail.com>
2021-11-14 22:58:08 +0330

    Fix issue on actual windows execution

--- tryMe.bat
+++ tryMe.bat
@@ -6,18 +6,16 @@ set /p l=Minimum of each answer:
 
 set /a t=0
 
-for /l %%x in (1, 0, 1) DO (
+for /l %%x in (1, 1, 10000) DO (
 	set /a a=1
 	set /a b=3
-	:loop
 	set /a a=!RANDOM! * !m!
 	set /a a=!a! / 32768 + 10 * !l!
 	set /a b=!a! / !l!
 	set /a b=!RANDOM! * !b! / 32768 + !l!
 	set /a r=!a!%%!b!
-	if not !r!==0 (goto loop)
-	set /p s= !a! // !b! : 
-	set /a r=!a!/!b!
-
-	if !r!==!s! (ECHO Correct! && set /a t=t+1
-	) ELSE (ECHO Wrong. The Answer is !r!))
+	if !r!==0 (
+		set /p s={!t!} !a! // !b! : 
+		set /a r=!a!/!b!
+		if !r!==!s! (ECHO Correct! && set /a t=t+1
+		) ELSE (ECHO Wrong. The Answer is !r!)))
